ca8de06d0cdd433b150a567d38f7b402355a0527
Markus Muschol <markus.muschol@gmx.de>
2019-07-01 19:27:29 +0200

    Properties

--- src/SciPhp/NdArray.php
+++ src/SciPhp/NdArray.php
@@ -7,12 +7,19 @@ use SciPhp\NdArray\Decorator;
 
 /**
  * Base array
+ *
+ * @property NdArray $T
+ * @property int $ndim
+ * @property int $size
+ * @property array $data
+ * @property array $shape
+ *
  */
 final class NdArray extends Decorator
 {
     /**
      * Constructor
-     * 
+     *
      * @param array $data
      */
     final public function __construct(array $data)
@@ -22,7 +29,7 @@ final class NdArray extends Decorator
 
     /**
      * Pretty printer
-     * 
+     *
      * @return string
      */
     final public function __toString()
